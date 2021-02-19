44cdf9e3ba9776d6cf426785f78a553911631a6d
naerim <naerim1119@gmail.com>
2021-02-19 21:08:43 +0900

    Background Gradient

--- Weather.js
+++ Weather.js
@@ -4,18 +4,61 @@ import PropTypes from "prop-types";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { LinearGradient } from "expo-linear-gradient";
 
-export default function Weather({ temp }) {
+const weatherOptions = {
+  Haze: {
+    iconName: "weather-hail",
+    gradient: ["#4DA0B0", "#D39D38"]
+  },
+  ThunderStorm: {
+    iconName: "weather-lightning",
+    gradient: ["#373B44", "#4286f4"]
+  },
+  Drizzle: {
+    iconName: "weather-hail",
+    gradient: ["#89F7FE", "#66A6FF"],
+  },
+  Rain: {
+    iconName: "weather-rainy",
+    gradient: ["#00C6FB", "#005BEA"],
+  },
+  Snow: {
+    iconName: "weather-snowy",
+    gradient: ["#7DE2FC", "#B9B6E5"],
+  },
+  Atmosphere: {
+    iconName: "weather-hail",
+    gradient: ["#89F7FE", "#66A6FF"],
+  },
+  Clear: {
+    iconName: "weather-sunny",
+    gradient: ["#FF7300", "#FEF253"],
+  },
+  Clouds: {
+    iconName: "weather-cloudy",
+    gradient: ["#D7D2CC", "#304352"],
+  },
+  Mist: {
+    iconName: "weather-hail",
+    gradient: ["#4DA0B0", "#D39D38"],
+  },
+  Dust: {
+    iconName: "weather-hail",
+    gradient: ["#4DA0B0", "#D39D38"],
+  },
+};
+
+export default function Weather({ temp, condition }) {
   return (
     <LinearGradient
-      colors={["#4c669f", "#3b5998", "#192f6a"]}
+      colors={weatherOptions[condition].gradient}
       style={styles.container}
     >
-      <StatusBar barStyle="light-content" />
+      <StatusBar barStyle=" light-content" />
       <Text style={styles.text}>Sign in with Facebook</Text>
       <View style={styles.halfContainer}>
         <MaterialCommunityIcons
           size={96}
-          name="weather-lightning-rainy"
+          name={weatherOptions[condition].iconName}
           color="white"
         />
         <Text style={styles.temp}>{temp}º</Text>
