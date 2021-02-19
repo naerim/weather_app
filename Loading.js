22a3214c0f3b6d28df3d1ea8864a6d8223f93f6f
naerim <naerim1119@gmail.com>
2021-02-19 20:47:58 +0900

    Add StatusBar

--- Loading.js
+++ Loading.js
@@ -1,23 +1,25 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, StatusBar } from "react-native";
 
-export default function Loading(){
-    return <View style={styles.container}>
-        <Text style={styles.text}>Getting the Beautiful weather</Text>
+export default function Loading() {
+  return (
+    <View style={styles.container}>
+      <StatusBar barStyle="dark-content" />
+      <Text style={styles.text}>Getting the Beautiful weather</Text>
     </View>
+  );
 }
 
-
 const styles = StyleSheet.create({
-    container: {
-        flex:1,
-        justifyContent: "flex-end",
-        paddingHorizontal: 30,
-        paddingVertical: 100,
-        backgroundColor: "#FdF6AA"
-    },
-    text: {
-        color: "#2C2C2C",
-        fontSize: 30
-    }
-})
+  container: {
+    flex: 1,
+    justifyContent: "flex-end",
+    paddingHorizontal: 30,
+    paddingVertical: 100,
+    backgroundColor: "#FdF6AA",
+  },
+  text: {
+    color: "#2C2C2C",
+    fontSize: 30,
+  },
+});
--- Weather.js
+++ Weather.js
@@ -1,17 +1,27 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, StatusBar } from "react-native";
 import PropTypes from "prop-types";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function Weather({ temp }) {
   return (
-    <View style={styles.container}>
+    <LinearGradient
+      colors={["#4c669f", "#3b5998", "#192f6a"]}
+      style={styles.container}
+    >
+      <StatusBar barStyle="light-content" />
+      <Text style={styles.text}>Sign in with Facebook</Text>
       <View style={styles.halfContainer}>
-        <MaterialCommunityIcons size={96} name="weather-lightning-rainy" />
-        <Text style={styles.temp}>{temp}℃</Text>
+        <MaterialCommunityIcons
+          size={96}
+          name="weather-lightning-rainy"
+          color="white"
+        />
+        <Text style={styles.temp}>{temp}º</Text>
       </View>
       <View style={styles.halfContainer}></View>
-    </View>
+    </LinearGradient>
   );
 }
 
@@ -39,6 +49,7 @@ const styles = StyleSheet.create({
   },
   temp: {
     fontSize: 42,
+    color: "white",
   },
   halfContainer: {
     flex: 1,
